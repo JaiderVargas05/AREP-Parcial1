3ada30ab45f23926a60589271c85b5c971358a81
unknown <jaider.vargas-n@labinfo.is.escuelaing.edu.co>
2025-09-10 08:59:20 -0500

    creacion proyecto

---
+++ src/main/java/escuelaing/edu/co/arep/parcial1/AREPParcial1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package escuelaing.edu.co.arep.parcial1;
+
+/**
+ *
+ * @author jaider.vargas-n
+ */
+public class AREPParcial1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
